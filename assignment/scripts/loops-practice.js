cbf3398c7735652f4d2be70b02172c4c73948cbb
AndrewZajac <andrewzajac05@gmail.com>
2024-04-03 16:34:57 -0500

    2a is done, created a for of loop for it

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,18 +13,33 @@ for (let i = 0; i < 4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+//I have to change the i<4 to i<6
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+//i must = 3 instead of 0, and i<6 and not i<4
+for (let i = 3; i < 6; i++) {
+  console.log(i);
+}
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+//I needed to change i to = 2, make it so i<11 and increase the increment to 2
+for (let i = 2; i < 11; i+= 2) {
+  console.log(i);
+}
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+//I had to set i to 5, make i greater than -1 and have i decrement until it reaches 0
+for (let i = 5; i > -1; i--) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -32,6 +47,10 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
+
+for(let group of stars){
+  console.log(group);
+}
 
 
 // 3. While loops  
