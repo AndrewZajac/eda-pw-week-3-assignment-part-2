106833ed2bc7f28705d5c1bde953a0e15abad722
AndrewZajac <andrewzajac05@gmail.com>
2024-04-03 17:24:23 -0500

    completed 3b and 3c, still need to do 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -59,8 +59,34 @@ console.log('---- 3. While loop ----');
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
 
+let i = 0;
+let text = '';
+while(stars[i]){
+  text += stars[i];
+  i++;
+  console.log(text);
+}
+//NEED HELP
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
 
+let index = 0;
+const max = 5;
+
+while(index < max){
+  console.log('in while loop. index:', index);
+  index ++;
+}
+console.log('index after while loop:', index);
+
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+index = 10;
+const min = 5;
+
+while(index > max){
+  console.log('in while loop. index:', index);
+  index --;
+}
+console.log('index after while loop:', index);
